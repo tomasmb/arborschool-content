5f201c75bb8d1a565dcfc86d728b0b48cfdc314c
Tomas Morales <tomas.morales.ber@gmail.com>
2026-02-04 13:02:21 -0300

    🧹 chore: remove completed spec docs and old feedback prototype
    - Delete docs/specifications/question-pipeline-redesign/ (all phases complete)
    - Delete examples/feedback_system/ (replaced by app/question_feedback/)

--- examples/feedback_system/__init__.py
+++
@@ -1,5 +0,0 @@
-# Feedback System Package
-from feedback_system.generator import FeedbackGenerator
-from feedback_system.validator import FeedbackValidator
-
-__all__ = ["FeedbackGenerator", "FeedbackValidator"]
